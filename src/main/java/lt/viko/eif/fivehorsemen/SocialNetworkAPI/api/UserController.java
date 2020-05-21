0787ada988633665c0ff378993b94591cf49d7d6
Laurynas.Zlatkus <lazlatkus@gmail.com>
2020-05-21 21:04:09 +0300

    Database connection

--- src/main/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/api/UserController.java
+++ src/main/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/api/UserController.java
@@ -1,5 +1,7 @@
 package lt.viko.eif.fivehorsemen.SocialNetworkAPI.api;
 
+import lt.viko.eif.fivehorsemen.SocialNetworkAPI.data.User;
+import lt.viko.eif.fivehorsemen.SocialNetworkAPI.database.MySqlConnection;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    MySqlConnection mySqlConnection = new MySqlConnection();
+
     @GetMapping(path = "/hello")
     public String test(){
         return "hello";
+    }
+
+    @GetMapping(path = "/db")
+    public String test2(){
+        return mySqlConnection.fetchUser().getName();
     }
 }
---
+++ src/main/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/database/MySqlConnection.java
@@ -0,0 +1,47 @@
+package lt.viko.eif.fivehorsemen.SocialNetworkAPI.database;
+
+import lt.viko.eif.fivehorsemen.SocialNetworkAPI.data.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySqlConnection {
+    static String USER = "lopai";
+    static String PASSWORD = "tumasonis";
+    static String DB_URL = "jdbc:mysql://78.61.168.194:3306/cityTransport";
+
+    private Connection connect () {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(DB_URL,USER,PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
+
+    public User fetchUser(){
+        Statement stmt = null;
+        User user = null;
+
+        try {
+            stmt = this.connect().createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT id, email, name, surname, password, phoneNumber, lastSeen," +
+                                                "dateOfBirth FROM User WHERE id = 3");
+
+            while (rs.next()) {
+                user = new User(rs.getString("id"), rs.getString("email"), rs.getString("name"),
+                        rs.getString("surname"), rs.getString("password"), rs.getString("phoneNumber"),
+                        rs.getString("lastSeen"), rs.getString("dateOfBirth"));
+            }
+
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+}
