e59e6a7d64d10574155f594bf9c510066ffeaac2
RytisRazmus <rytis.razmus@gmail.com>
2020-05-21 20:07:03 +0300

    Squashed commit of the following:
    commit 2a9f63204d94753fda083fb34f1b981429a2b4fe
    Author: RytisRazmus <rytis.razmus@gmail.com>
    Date:   Thu May 21 20:05:53 2020 +0300
    
        added User object

--- src/main/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/api/UserController.java
+++ src/main/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/api/UserController.java
@@ -1,11 +1,9 @@
 package lt.viko.eif.fivehorsemen.SocialNetworkAPI.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.nio.file.Path;
 @RequestMapping("User")
 @RestController
 public class UserController {
---
+++ src/main/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/data/User.java
@@ -0,0 +1,56 @@
+package lt.viko.eif.fivehorsemen.SocialNetworkAPI.data;
+
+public class User {
+    private String id;
+    private String email;
+    private String name;
+    private String surname;
+    private String phoneNumber;
+    private String lastSeen;
+    private String dateOfBirth;
+    private String password;
+
+    public User(String id, String email, String name, String surname, String phoneNumber,
+                String lastSeen, String dateOfBirth, String password) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
+        this.lastSeen = lastSeen;
+        this.dateOfBirth = dateOfBirth;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getLastSeen() {
+        return lastSeen;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
