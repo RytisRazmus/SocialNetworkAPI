2a9f63204d94753fda083fb34f1b981429a2b4fe
RytisRazmus <rytis.razmus@gmail.com>
2020-05-21 20:05:53 +0300

    added User object

--- src/main/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/SocialNetworkApiApplication.java
+++ src/main/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/SocialNetworkApiApplication.java
@@ -1,12 +1,9 @@
 package lt.viko.eif.fivehorsemen.SocialNetworkAPI;
 
-import lt.viko.eif.fivehorsemen.SocialNetworkAPI.api.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan(basePackageClasses = UserController.class)
 public class SocialNetworkApiApplication {
 
 	public static void main(String[] args) {
