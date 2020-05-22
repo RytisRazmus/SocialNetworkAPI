51dd6a6c7285c3624a241bc29866bd2622f19ae7
RytisRazmus <rytis.razmus@gmail.com>
2020-05-22 16:51:02 +0300

    added repository method bodies

--- src/main/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/repository/APIRepository.java
+++ src/main/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/repository/APIRepository.java
@@ -1,4 +1,18 @@
 package lt.viko.eif.fivehorsemen.SocialNetworkAPI.repository;
 
+import lt.viko.eif.fivehorsemen.SocialNetworkAPI.data.*;
+
+import java.util.ArrayList;
+
 public interface APIRepository {
+    User getUser(String email, String password);
+    void deleteFriendInv(String id);
+    Friend searchUser(String fullname);
+    void acceptFriendInvite(String toUser, String fromUser);
+    ArrayList<FriendPost> getFriendPosts(String userId);
+    ArrayList<FriendInvite> getFriendInvites(String userId);
+    boolean addUser(User user);
+    boolean insertFriendInvite(String toUser, String fromUser);
+    ArrayList<Friend> getFriends(String userId);
+    boolean addPost(Post post);
 }
--- src/main/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/repository/APIRepositoryImpl.java
+++ src/main/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/repository/APIRepositoryImpl.java
@@ -1,4 +1,57 @@
 package lt.viko.eif.fivehorsemen.SocialNetworkAPI.repository;
 
-public class APIRepositoryImpl {
+import lt.viko.eif.fivehorsemen.SocialNetworkAPI.data.*;
+
+import java.util.ArrayList;
+
+public class APIRepositoryImpl implements APIRepository {
+    @Override
+    public User getUser(String email, String password) {
+        return null;
+    }
+
+    @Override
+    public void deleteFriendInv(String id) {
+
+    }
+
+    @Override
+    public Friend searchUser(String fullname) {
+        return null;
+    }
+
+    @Override
+    public void acceptFriendInvite(String toUser, String fromUser) {
+
+    }
+
+    @Override
+    public ArrayList<FriendPost> getFriendPosts(String userId) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<FriendInvite> getFriendInvites(String userId) {
+        return null;
+    }
+
+    @Override
+    public boolean addUser(User user) {
+        return false;
+    }
+
+    @Override
+    public boolean insertFriendInvite(String toUser, String fromUser) {
+        return false;
+    }
+
+    @Override
+    public ArrayList<Friend> getFriends(String userId) {
+        return null;
+    }
+
+    @Override
+    public boolean addPost(Post post) {
+        return false;
+    }
 }
