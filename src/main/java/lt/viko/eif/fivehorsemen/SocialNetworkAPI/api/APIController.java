b8c82e88dbcecc7295e497681a52c7df3eb52f68
Laurynas.Zlatkus <lazlatkus@gmail.com>
2020-05-22 18:27:43 +0300

    More resources addded

--- src/main/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/api/APIController.java
+++ src/main/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/api/APIController.java
@@ -1,10 +1,7 @@
 package lt.viko.eif.fivehorsemen.SocialNetworkAPI.api;
 
 
-import lt.viko.eif.fivehorsemen.SocialNetworkAPI.data.Friend;
-import lt.viko.eif.fivehorsemen.SocialNetworkAPI.data.FriendInvite;
-import lt.viko.eif.fivehorsemen.SocialNetworkAPI.data.Post;
-import lt.viko.eif.fivehorsemen.SocialNetworkAPI.data.User;
+import lt.viko.eif.fivehorsemen.SocialNetworkAPI.data.*;
 import lt.viko.eif.fivehorsemen.SocialNetworkAPI.repository.APIRepositoryImpl;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,30 +15,56 @@ public class APIController {
 
     private APIRepositoryImpl repository = new APIRepositoryImpl();
 
-    @GetMapping(path = "/getFriendInvites")
+    @GetMapping(path = "/friendInvites")
     public ArrayList<FriendInvite> getFriendInvites(@RequestParam("id") String userId){
         return repository.getFriendInvites(userId);
     }
 
-    @PostMapping(path = "/addUser")
+    @PostMapping(path = "/users")
     public boolean addUser(@RequestBody User user){
         return repository.addUser(user);
     }
 
-    @PostMapping(path = "/sendFriendInvite")
+    @PostMapping(path = "/friendInvites")
     public boolean sendFriendInvite(@RequestBody Map<String, String> json){
         String toUser = json.get("toUser");
         String fromUser = json.get("fromUser");
         return repository.insertFriendInvite(toUser, fromUser);
     }
 
-    @GetMapping(path = "/getFriends")
+    @GetMapping(path = "/friends")
     public ArrayList<Friend> getFriends(@RequestParam("id") String userId){
         return repository.getFriends(userId);
     }
 
-    @PostMapping(path = "/addPost")
+    @PostMapping(path = "/posts")
     public boolean addPost(@RequestBody Post post){
         return repository.addPost(post);
     }
+
+    @GetMapping(path = "/users")
+    public User getUser(@RequestParam(name = "email") String email, @RequestParam(name = "pass") String pass){
+        return repository.getUser(email, pass);
+    }
+
+    @GetMapping(path = "/friend")
+    public Friend searchForFriend(@RequestParam(name = "fullname") String fullname){
+        return repository.searchUser(fullname);
+    }
+
+    @DeleteMapping(path = "/friendInvites")
+    public void deleteFriendInv(@RequestParam(name = "id") String id){
+        repository.deleteFriendInv(id);
+    }
+
+    @PostMapping(path = "/friends")
+    public void acceptFriend(@RequestParam(name = "toUser") String toUser, @RequestParam(name = "fromUser") String fromUser){
+        repository.acceptFriendInvite(toUser,fromUser);
+    }
+
+    @GetMapping(path = "/posts")
+    public ArrayList<FriendPost> posts(@RequestParam(name = "id") String id) {
+        return repository.getFriendPosts(id);
+    }
+
 }
