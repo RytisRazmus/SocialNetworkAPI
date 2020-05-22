4a804517331f70074e9910ffddcd2b0e615dce99
Laurynas.Zlatkus <lazlatkus@gmail.com>
2020-05-22 14:56:20 +0300

    Modified UserController

--- src/main/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/api/UserController.java
+++ src/main/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/api/UserController.java
@@ -1,37 +1,42 @@
 package lt.viko.eif.fivehorsemen.SocialNetworkAPI.api;
 
+import lt.viko.eif.fivehorsemen.SocialNetworkAPI.data.Friend;
+import lt.viko.eif.fivehorsemen.SocialNetworkAPI.data.FriendPost;
 import lt.viko.eif.fivehorsemen.SocialNetworkAPI.data.User;
 import lt.viko.eif.fivehorsemen.SocialNetworkAPI.database.MySqlConnection;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("User")
+import java.util.List;
+
+@RequestMapping("users")
 @RestController
+@ResponseBody
 public class UserController {
 
     MySqlConnection mySqlConnection = new MySqlConnection();
 
-    @GetMapping(path = "/hello")
-    public String test(){
-        return "hello";
-    }
-
     @GetMapping(path = "/user")
-    public String user(){
-        return mySqlConnection.getUser("laurynas.zlatkus@gmail.com", "123456").getName();
+    public User user(@RequestParam(name = "email") String email, @RequestParam(name = "pass") String pass){
+        return mySqlConnection.getUser(email, pass);
     }
 
     @GetMapping(path = "/search")
-    public String search(){
-        return mySqlConnection.searchUser("rytis razmus").getSurname();
+    public Friend search(@RequestParam(name = "fullname") String fullname){
+        return mySqlConnection.searchUser(fullname);
     }
 
     @DeleteMapping(path = "/del")
-    public void del(){
-        mySqlConnection.deleteFriendInv("33");
+    public void del(@RequestParam(name = "id") String id){
+        mySqlConnection.deleteFriendInv(id);
     }
 
     @PostMapping(path = "/friend")
     public void friend(){
         mySqlConnection.acceptFriendInvite("8","9");
+    }
+
+    @GetMapping(path = "/posts")
+    public List<FriendPost> posts() {
+        return mySqlConnection.getFriendPosts("3");
     }
 }
--- src/main/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/database/MySqlConnection.java
+++ src/main/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/database/MySqlConnection.java
@@ -1,6 +1,7 @@
 package lt.viko.eif.fivehorsemen.SocialNetworkAPI.database;
 
 import lt.viko.eif.fivehorsemen.SocialNetworkAPI.data.Friend;
+import lt.viko.eif.fivehorsemen.SocialNetworkAPI.data.FriendPost;
 import lt.viko.eif.fivehorsemen.SocialNetworkAPI.data.User;
 
 import java.awt.*;
@@ -26,7 +27,7 @@ public class MySqlConnection {
 
     public User getUser(String email, String password){
         String statement = "SELECT id, email, name, surname, password, phoneNumber, lastSeen," +
-                                    "dateOfBirth FROM User WHERE email = ? AND password = ?";
+                            "dateOfBirth FROM User WHERE email = ? AND password = ?";
         User user = null;
 
         try {
@@ -77,11 +78,11 @@ public class MySqlConnection {
 
     public Friend searchUser(String fullname) {
         String statement = "SELECT user.id, user.name, user.surname, img.imageUrl as imageUrl " +
-        "FROM User user " +
-        "LEFT JOIN UserImage usrImg ON (usrImg.userId = user.id) " +
-        "LEFT JOIN Image img ON (img.id = usrImg.image) " +
-        "AND usrImg.isPrimaryImage = 1 "+
-        "WHERE user.fullName LIKE ?";
+                            "FROM User user " +
+                            "LEFT JOIN UserImage usrImg ON (usrImg.userId = user.id) " +
+                            "LEFT JOIN Image img ON (img.id = usrImg.image) " +
+                            "AND usrImg.isPrimaryImage = 1 "+
+                            "WHERE user.fullName LIKE ?";
 
         Friend friend = null;
 
@@ -133,6 +134,47 @@ public class MySqlConnection {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public List<FriendPost> getFriendPosts(String userId) {
+        String statement = "SELECT post.date, f2.name, f2.surname, f2.id, im.imageURL as 'profileImage', content.description," +
+                            "image.imageUrl FROM Post post INNER JOIN Content content ON (post.contentId = content.id) " +
+                            "LEFT JOIN Image image ON (image.contentId = content.id) " +
+                            "INNER JOIN User user ON (user.id = ?) " +
+                            "INNER JOIN Friend friend ON friend.userId = user.id " +
+                            "INNER JOIN (SELECT * FROM User) f2 ON friend.friendid = f2.id " +
+                            "INNER JOIN UserImage uImage ON friendId = uImage.userId " +
+                            "INNER JOIN Image im on uImage.image = im.id && uImage.isPrimaryImage = 1 " +
+                            "WHERE friend.friendId = post.userId " +
+                            "ORDER BY post.date DESC";
+
+        List<FriendPost> friendsPosts = new ArrayList<>();
+
+        try {
+            Connection conn = this.connect();
+
+            PreparedStatement pstmt = conn.prepareStatement(statement);
+
+            pstmt.setString(1, userId);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                friendsPosts.add(new FriendPost(rs.getDate("post.date"), rs.getString("f2.name"),
+                                                rs.getString("f2.surname"),rs.getString("f2.id"),
+                                                rs.getString("content.description"),rs.getString("image.imageUrl"),
+                                                rs.getString("profileImage")));
+            }
+
+            rs.close();
+            pstmt.close();
+            conn.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return friendsPosts;
+
     }
 
 
