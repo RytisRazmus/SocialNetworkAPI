b4e0d1077c7118c121e7efd0d9e317d5f08aa9ab
Laurynas.Zlatkus <lazlatkus@gmail.com>
2020-05-23 20:35:38 +0300

    edited imports

--- src/main/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/exception/RestErrorHandler.java
+++ src/main/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/exception/RestErrorHandler.java
@@ -1,12 +1,11 @@
 package lt.viko.eif.fivehorsemen.SocialNetworkAPI.exception;
 
-import org.springframework.http.HttpHeaders;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
