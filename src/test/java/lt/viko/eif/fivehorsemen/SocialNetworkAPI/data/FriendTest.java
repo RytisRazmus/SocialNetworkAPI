6d68f3ba52e18a36cb07ebdd5963a98f2bd40b59
Evaldas189 <evaldas.tam1@gmail.com>
2020-05-23 07:09:34 -0900

    taip

--- src/test/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/data/FriendTest.java
+++ src/test/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/data/FriendTest.java
@@ -1,24 +1,43 @@
 package lt.viko.eif.fivehorsemen.SocialNetworkAPI.data;
 
+import lt.viko.eif.fivehorsemen.SocialNetworkAPI.repository.APIRepositoryImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FriendTest {
 
+    private Friend friend;
+    @BeforeEach
+    void setUp(){
+        friend = new Friend("1","Evaldas","Tamutis",
+                "https://www.cutoutme.com.au/wp-content/uploads/2018/07/Single-CHls.jpg");
+    }
+
     @Test
     void getId() {
+
+        String response = friend.getId();
+        assertEquals("1",response);
     }
 
     @Test
     void getName() {
+        String response = friend.getName();
+        assertEquals("Evaldas",response);
     }
 
     @Test
     void getSurname() {
+        String response = friend.getSurname();
+        assertEquals("Tamutis",response);
     }
 
     @Test
     void getImageUrl() {
+        String response = friend.getImageUrl();
+        assertEquals("https://www.cutoutme.com.au/wp-content/uploads/2018/07/Single-CHls.jpg",response);
     }
 }
