e91664c435e05ee80568cc223237826a42571c02
Evaldas189 <evaldas.tam1@gmail.com>
2020-05-23 03:39:15 -0900

    sw all cia as pradesiu ok?

---
+++ src/test/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/data/FriendTest.java
@@ -0,0 +1,24 @@
+package lt.viko.eif.fivehorsemen.SocialNetworkAPI.data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FriendTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getSurname() {
+    }
+
+    @Test
+    void getImageUrl() {
+    }
+}
