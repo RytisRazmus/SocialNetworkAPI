c2cb53985379e70a6a6e62adfbeb52d008228d8a
Evaldas189 <evaldas.tam1@gmail.com>
2020-05-24 00:34:48 -0900

    finished FriendPost tests

--- src/test/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/data/FriendPostTest.java
+++ src/test/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/data/FriendPostTest.java
@@ -2,7 +2,12 @@ package lt.viko.eif.fivehorsemen.SocialNetworkAPI.data;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.hateoas.Link;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -14,11 +19,11 @@ import java.util.Date;
 import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 class FriendPostTest {
 
     FriendPost friendPost;
-
     @BeforeEach
     void setUp() throws ParseException {
         DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
@@ -29,6 +34,10 @@ class FriendPostTest {
                 "Maciau gera pana.","https://upload.wikimedia.org/wikipedia/commons/0/0c/Cow" +
                 "_female_black_white.jpg","https://i.pinimg.com/474x/ff/5d/06/ff5d0624c089399d5736f8ce" +
                 "0cc5ebeb.jpg");
+
+        HttpServletRequest httpServletRequestMock = new MockHttpServletRequest();
+        ServletRequestAttributes servletRequestAttributes = new ServletRequestAttributes(httpServletRequestMock);
+        RequestContextHolder.setRequestAttributes(servletRequestAttributes);
     }
 
     @Test
@@ -41,13 +50,34 @@ class FriendPostTest {
 
     @Test
     void getProfileImage() {
+        String response = friendPost.getProfileImage();
+        assertEquals("https://i.pinimg.com/474x/ff/5d/06/ff5d0624c089399d5736f8ce0cc5ebeb.jpg",response);
     }
 
     @Test
     void getName() {
+        String response = friendPost.getName();
+        assertEquals("Laurynas",response);
     }
 
     @Test
     void getSurname() {
+        String response = friendPost.getSurname();
+        assertEquals("Zlatkus",response);
+    }
+
+    @Test
+    void getLink(){
+        Link link = linkTo(FriendPost.class).slash("/api/posts").withSelfRel();
+        friendPost.setLink(link);
+        Link response = friendPost.getLink();
+        assertEquals(link,response);
+    }
+
+    @Test
+    void setLink(){
+        Link link = linkTo(FriendPost.class).slash("/api/posts").withSelfRel();
+        friendPost.setLink(link);
+        assertEquals(link,friendPost.getLink());
     }
 }
