0fbaf3a64421f030cf95042ea2ccdf25da884fd1
JonasPonas <zemaitis.jonas0@gmail.com>
2020-05-25 14:13:10 +0300

    modified FriendInviteTest

--- src/test/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/data/FriendInviteTest.java
+++ src/test/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/data/FriendInviteTest.java
@@ -2,18 +2,23 @@ package lt.viko.eif.fivehorsemen.SocialNetworkAPI.data;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.hateoas.Link;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
 public class FriendInviteTest {
+
+    private FriendInvite friendInv;
 
     @BeforeEach
     void setUp(){
-        //friend = new Friend("1","Evaldas","Tamutis",
-        //        "https://www.cutoutme.com.au/wp-content/uploads/2018/07/Single-CHls.jpg");
+        friendInv = new FriendInvite("9", "Not Bruce", "Not Wayne", null, "36");
         HttpServletRequest httpServletRequestMock = new MockHttpServletRequest();
         ServletRequestAttributes servletRequestAttributes = new ServletRequestAttributes(httpServletRequestMock);
         RequestContextHolder.setRequestAttributes(servletRequestAttributes);
@@ -21,13 +26,22 @@ public class FriendInviteTest {
 
     @Test
     public void getInviteId() {
+        String response = friendInv.getInviteId();
+        assertEquals("36",response);
     }
 
     @Test
     public void getLink() {
+        Link link = linkTo(Friend.class).slash("/api/friendInvites").withSelfRel();
+        friendInv.setLink(link);
+        Link response = friendInv.getLink();
+        assertEquals(link,response);
     }
 
     @Test
     public void setLink() {
+        Link link = linkTo(Friend.class).slash("/api/friendInvites").withSelfRel();
+        friendInv.setLink(link);
+        assertEquals(link,friendInv.getLink());
     }
 }
