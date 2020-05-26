9f962fe7fa94ea57b04e75387330a417c21c5cb5
Evaldas189 <evaldas.tam1@gmail.com>
2020-05-26 00:01:59 -0900

    APIrepositoryImpl tests continue

--- src/test/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/repository/APIRepositoryImplTest.java
+++ src/test/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/repository/APIRepositoryImplTest.java
@@ -1,5 +1,7 @@
 package lt.viko.eif.fivehorsemen.SocialNetworkAPI.repository;
 import lt.viko.eif.fivehorsemen.SocialNetworkAPI.data.Friend;
+import lt.viko.eif.fivehorsemen.SocialNetworkAPI.data.FriendInvite;
+import lt.viko.eif.fivehorsemen.SocialNetworkAPI.data.FriendPost;
 import lt.viko.eif.fivehorsemen.SocialNetworkAPI.data.User;
 import lt.viko.eif.fivehorsemen.SocialNetworkAPI.database.MySqlConnection;
 import org.junit.jupiter.api.Test;
@@ -12,7 +14,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.Link;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -61,14 +67,47 @@ class APIRepositoryImplTest {
 
     @Test
     void acceptFriendInvite() {
+        String fromUser = "Evaldas";
+        String toUser = "Valanciunas";
+        when(mySqlConnection.acceptFriendInvite(toUser,fromUser)).thenReturn(true);
+        boolean result = repository.acceptFriendInvite(toUser,fromUser);
+        assertThat(result).isTrue();
     }
 
     @Test
-    void getFriendPosts() {
+    void getFriendPosts() throws ParseException {
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date myDate = format.parse("2020-05-14");
+        String dmy = format.format(myDate);
+        FriendPost friendPost = new FriendPost(myDate, "Laurynas", "Zlatkus", "1",
+                "Maciau gera pana.", "https://upload.wikimedia.org/wikipedia/commons/0/0c/Cow" +
+                "_female_black_white.jpg", "https://i.pinimg.com/474x/ff/5d/06/ff5d0624c089399d5736f8ce" +
+                "0cc5ebeb.jpg");
+        FriendPost friendPost1 = new FriendPost(myDate, "Evaldas", "Tamutis", "2",
+                "Maciau gera pana.", "https://upload.wikimedia.org/wikipedia/commons/0/0c/Cow" +
+                "_female_black_white.jpg", "https://i.pinimg.com/474x/ff/5d/06/ff5d0624c089399d5736f8ce" +
+                "0cc5ebeb.jpg");
+        ArrayList<FriendPost> posts = new ArrayList<>();
+        posts.add(friendPost);
+        posts.add(friendPost1);
+        when(mySqlConnection.getFriendPosts("1")).thenReturn(posts);
+        ArrayList<FriendPost> result = repository.getFriendPosts("1");
+        assertEquals(result,posts);
     }
 
     @Test
     void getFriendInvites() {
+        FriendInvite friendInv = new FriendInvite("1", "Evaldas", "Tamutis", "https://" +
+                "www.cutoutme.com.au/wp-content/uploads/2018/07/Single-CHls.jpg", "1");
+        FriendInvite friendInv1 = new FriendInvite("2", "Elon", "Musk", "https://" +
+                "www.cutoutme.com.au/wp-content/uploads/2018/07/Single-CHls.jpg", "2");
+        ArrayList<FriendInvite> friendInvites = new ArrayList<>();
+        friendInvites.add(friendInv);
+        friendInvites.add(friendInv1);
+        when(mySqlConnection.getFriendInvites("1")).thenReturn(friendInvites);
+        ArrayList<FriendInvite> result = repository.getFriendInvites("1");
+        assertThat(result.size()).isEqualTo(2);
+        assertEquals(result, friendInvites);
     }
 
     @Test
