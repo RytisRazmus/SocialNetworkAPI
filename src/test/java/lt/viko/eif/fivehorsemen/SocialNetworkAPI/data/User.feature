0cd01e37a8e69b4ab1c69b921b6868037d754c90
Laurynas.Zlatkus <lazlatkus@gmail.com>
2020-06-12 13:08:54 +0300

    User cucumnber

---
+++ src/test/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/data/User.feature
@@ -0,0 +1,6 @@
+Feature: User
+
+  Scenario: Create new User
+    Given Have a parameters
+    When Add parameters to constructor
+    Then New User created
---
+++ src/test/java/lt/viko/eif/fivehorsemen/SocialNetworkAPI/data/UserStepdefs.java
@@ -0,0 +1,26 @@
+package lt.viko.eif.fivehorsemen.SocialNetworkAPI.data;
+
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class UserStepdefs {
+    @Given("Have a parameters")
+    public void haveAParameters() {
+
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("Add parameters to constructor")
+    public void addParametersToConstructor() {
+
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("New User created")
+    public void newUserCreated(){
+
+        throw new io.cucumber.java.PendingException();
+    }
+}
